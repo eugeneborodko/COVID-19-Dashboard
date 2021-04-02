3d1f321e78b0a163874f163f8e424609d74d7784
evgenborod496 <eugeneborodko@gmail.com>
2021-04-02 12:01:58 +0300

    feat: add bar chart for Total Confirmed values

--- src/components/BarChart/BarChart.js
+++ src/components/BarChart/BarChart.js
@@ -8,24 +8,41 @@ const BarChart = () => {
       <Bar
         data={{
           labels: [
-            'January',
-            'February',
-            'March',
-            'April',
-            'May',
-            'June',
-            'July',
-            'August',
-            'September',
-            'October',
-            'November',
-            'December',
+            'Americas',
+            'Europe',
+            'South-East Asia',
+            'Eastern Mediterranean',
+            'Africa',
+            'Western Pacific',
+          ],
+          datasets: [
+            {
+              label: 'Confirmed Cases (2020)',
+              data: [55941329, 45112584, 14911540, 7564704, 3097533, 1912547],
+              backgroundColor: [
+                'rgba(255, 99, 132)',
+                'rgba(54, 162, 235)',
+                'rgba(255, 206, 86)',
+                'rgba(75, 192, 192)',
+                'rgba(153, 102, 255)',
+                'rgba(255, 159, 64)',
+              ],
+            },
           ],
         }}
         width={600}
         height={600}
         options={{
           maintainAspectRatio: false,
+          scales: {
+            yAxes: [
+              {
+                ticks: {
+                  beginAtZero: true,
+                },
+              },
+            ],
+          },
         }}
       />
     </div>
